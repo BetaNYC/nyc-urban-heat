a91c04b6337b9da9b41ba3d2216bfa725886304d
CloudLun <cloud830902@gmail.com>
2024-06-19 17:34:20 -0400

    add content in the about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ export default {
   theme: {
     extend: {
       fontSize: {
+        headline: "1.625rem",
+        subheadline: "1.125rem",
         large: "1rem",
         regular: "0.875rem",
         small: "0.75rem",
