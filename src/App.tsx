9804a0346477f4d62455b980d1aa4590529e576b
CloudLun <cloud830902@gmail.com>
2024-06-27 10:56:53 -0400

    add surface temp pmtile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { MapProvider } from './contexts/mapContexts';
 
 import MapPage from './pages/MapPage';
 import AboutPage from './pages/AboutPage';
@@ -10,22 +14,27 @@ import DownloadPage from './pages/DownloadPage';
 
 function App() {
 
+  const queryClient = new QueryClient()
+
   useEffect(() => {
     document.title = 'NYC Urban Heat Portal'
   })
 
   return (
-    <main className='relative w-[100vw] h-[100vh] overflow-y-scroll'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/nyc-urban-heat/' element={<MapPage />} />
-          <Route path='/nyc-urban-heat/resources' element={<ResourcesPage />} />
-          <Route path='/nyc-urban-heat/about' element={<AboutPage />} />
-          <Route path='/nyc-urban-heat/download' element={<DownloadPage />} />
-        </Routes>
-      </BrowserRouter>
-
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className='relative w-[100vw] h-[100vh] overflow-y-scroll'>
+        <MapProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/nyc-urban-heat/' element={<MapPage />} />
+              <Route path='/nyc-urban-heat/resources' element={<ResourcesPage />} />
+              <Route path='/nyc-urban-heat/about' element={<AboutPage />} />
+              <Route path='/nyc-urban-heat/download' element={<DownloadPage />} />
+            </Routes>
+          </BrowserRouter>
+        </MapProvider>
+      </main >
+    </QueryClientProvider>
   );
 }
 
