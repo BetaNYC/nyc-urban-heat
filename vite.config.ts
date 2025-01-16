bc0b1c6d1ed8a7afbe480bcda39f8866ef1fe2af
CloudLun <cloud830902@gmail.com>
2025-01-16 08:38:05 -0500

    set up the custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// nyc-urban-heat
 export default defineConfig({
-  base: "/nyc-urban-heat",
+  base: "/",
   plugins: [
     react({
       babel: {
